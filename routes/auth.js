1b869480f2ff3c7ddabed6e8b21b9b291976d9a0
Jasminet <jastangting@gmail.com>
2023-11-19 15:05:29 -0800

    added 2fa verification

--- routes/auth.js
+++ routes/auth.js
@@ -2,10 +2,11 @@ const express = require('express')
 
 const router = express.Router()
 // const { route } = require('express/lib/application')
-const { test, login, register } = require('../controllers/auth')
+const { test, login, register, verify } = require('../controllers/auth')
 
 router.route('/test').get(test)
 router.route('/login').post(login)
 router.route('/register').post(register)
+router.route('/verify').post(verify)
 
 module.exports = router
--- server.js
+++ server.js
@@ -3,11 +3,9 @@ require("dotenv").config();
 const express = require("express");
 const mongoose= require("mongoose");
 
-
 const app = express();
-const DB_URI = process.env.DB_URI;
+const DB_URI = process.env.DB_URI || "mongodb://localhost:27017";;
 const PORT = process.env.PORT || 3001;
-
 
 // app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
@@ -16,6 +14,7 @@ app.use(express.static(path.join(__dirname, 'frontend/dist')))
 
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/user", require("./routes/private"));
+
 // handle all other route
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname + "/frontend/dist/index.html"));
