7260484c5049059d9f35e3c496bf2ee5fc1cc0fc
stevesun <sunguoqin@oneinfitech.com>
2023-11-20 16:21:29 -0800

    update functions

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,24 @@ import { useState } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [entries, setEntries] = useState([]);
+    const [formData, setFormData] = useState({
+        website: '',
+        username: '',
+        email: '',
+        password: '',
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault(); // Prevent form submission from reloading the page
+        setEntries([...entries, formData]);
+        setFormData({ website: '', username: '', email: '', password: '' }); // Clear the form
+    };
 
   return (
     <>
@@ -13,77 +31,67 @@ function App() {
           <button className="logout-button">Logout</button>
         </header>
         <div className="password-form-container">
-          <div className="form-header">Add Password</div>
-          <div className="form-fields">
-            <input type="text" placeholder="Website" className="form-input" />
-            <input type="text" placeholder="UserName" className="form-input" />
-            <input type="text" placeholder="Email" className="form-input" />
-            <input
-              type="password"
-              placeholder="Password"
-              className="form-input"
-            />
-            <button className="form-button">Add</button>
-          </div>
-        </div>
-        <div className="password-list">
-          <table>
-            <thead>
-              <tr>
-                <th>S.L</th>
-                <th>Website</th>
-                <th>UserName</th>
-                <th>Email</th>
-                <th>Password</th>
-                <th>Action</th>
-              </tr>
-            </thead>
-            <tbody>
-              {/* Repeat the row below for each password entry */}
-              <tr>
-                <td>1</td>
-                <td>Facebook</td>
-                <td>Bob</td>
-                <td>marifm1986@gmail.com</td>
-                <td>********</td>
-                <td>
-                  <button>X</button>
-                </td>
-              </tr>
-              <tr>
-                <td>1</td>
-                <td>Facebook</td>
-                <td>Bob</td>
-                <td>marifm1986@gmail.com</td>
-                <td>********</td>
-                <td>
-                  <button>X</button>
-                </td>
-              </tr>
-              <tr>
-                <td>1</td>
-                <td>Facebook</td>
-                <td>Bob</td>
-                <td>marifm1986@gmail.com</td>
-                <td>********</td>
-                <td>
-                  <button>X</button>
-                </td>
-              </tr>
-              <tr>
-                <td>1</td>
-                <td>Facebook</td>
-                <td>Bob</td>
-                <td>marifm1986@gmail.com</td>
-                <td>********</td>
-                <td>
-                  <button>X</button>
-                </td>
-              </tr>
-              {/* ... */}
-            </tbody>
-          </table>
-        </div>
+                <div className="form-header">Add Password</div>
+                <form className="form-fields" onSubmit={handleSubmit}>
+                    <input
+                        type="text"
+                        placeholder="Website"
+                        className="form-input"
+                        name="website"
+                        value={formData.website}
+                        onChange={handleChange}
+                    />
+                    <input
+                        type="text"
+                        placeholder="UserName"
+                        className="form-input"
+                        name="username"
+                        value={formData.username}
+                        onChange={handleChange}
+                    />
+                    <input
+                        type="text"
+                        placeholder="Email"
+                        className="form-input"
+                        name="email"
+                        value={formData.email}
+                        onChange={handleChange}
+                    />
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        className="form-input"
+                        name="password"
+                        value={formData.password}
+                        onChange={handleChange}
+                    />
+                    <button type="submit" className="form-button">Add</button>
+                </form>
+            </div>
+            <div className="password-list">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>S.L</th>
+                            <th>Website</th>
+                            <th>Username</th>
+                            <th>Email</th>
+                            <th>Password</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {entries.map((entry, index) => (
+                            <tr key={index}>
+                                <td>{index + 1}</td>
+                                <td>{entry.website}</td>
+                                <td>{entry.username}</td>
+                                <td>{entry.email}</td>
+                                <td>{'*'.repeat(entry.password.length)}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
       </div>
       <div className="card">
         <button
