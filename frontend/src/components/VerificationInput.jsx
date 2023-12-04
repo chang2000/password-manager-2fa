6aca54f859efd6c5aa14c5c54fc0de45e321257e
Tianchang Wang <wangtcalex@gmail.com>
2023-12-03 16:14:03 -0800

    feat: better input box

--- frontend/src/components/VerificationInput.jsx
+++ frontend/src/components/VerificationInput.jsx
@@ -1,26 +1,42 @@
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { Input, Button, HStack } from '@chakra-ui/react';
 
 // eslint-disable-next-line react/prop-types
-function VerificationInput({onInputComplete}) {
+function VerificationInput({ onInputComplete }) {
   const [digits, setDigits] = useState(Array(6).fill(''));
+  const inputRefs = useRef([]);
 
-  const handleChange = (value, index) => {
+  useEffect(() => {
+    if (inputRefs.current[0]) {
+      inputRefs.current[0].focus();
+    }
+  }, []);
+
+  const handleChange = (event, index) => {
+    const { value } = event.target;
     const newDigits = [...digits];
+
     newDigits[index] = value.slice(-1); // Get the last character
     setDigits(newDigits);
 
-    // Auto-focus to next input field if there's a next one and current value is filled
     if (value && index < 5) {
-      document.getElementById(`digit-${index + 1}`).focus();
+      inputRefs.current[index + 1].focus();
     }
   };
 
+  const handleKeyDown = (event, index) => {
+    if (event.key === 'Backspace' && index > 0 && !digits[index]) {
+      inputRefs.current[index - 1].focus();
+    } else if (event.key === 'Enter' && index === 5 && digits.join('').length === 6) {
+      handleSubmit();
+    }
+  }
+
   const handleSubmit = () => {
     const verificationCode = digits.join('');
-    if (onInputComplete)
-      onInputComplete(verificationCode); // Invoke the callback with the input value
-
+    if (onInputComplete) {
+      onInputComplete(verificationCode);
+    }
   };
 
   return (
@@ -29,9 +45,11 @@ function VerificationInput({onInputComplete}) {
         {digits.map((digit, index) => (
           <Input
             key={index}
+            ref={el => inputRefs.current[index] = el}
             id={`digit-${index}`}
             value={digit}
-            onChange={(e) => handleChange(e.target.value, index)}
+            onChange={(e) => handleChange(e, index)}
+            onKeyDown={(e) => handleKeyDown(e, index)}
             maxLength={1}
             size="lg"
             textAlign="center"
