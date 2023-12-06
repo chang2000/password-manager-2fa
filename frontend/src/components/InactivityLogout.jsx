ffea27c47d4224b9d55cf5ddefeec9ab2016f2b2
stevesun <sunguoqin@oneinfitech.com>
2023-12-06 12:09:54 -0800

    inactivity logout after 30 seconds

---
+++ frontend/src/components/InactivityLogout.jsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useToast } from '@chakra-ui/react';
+
+const InactivityLogout = () => {
+    const [inactiveTimer, setInactiveTimer] = useState(null);
+    const [warningTimer, setWarningTimer] = useState(null);
+    const navigate = useNavigate();
+    const toast = useToast();
+
+    const logoutUser = () => {
+        navigate('/');
+    };
+
+    const showWarning = () => {
+        toast({
+            title: 'Inactivity detected',
+            description: 'You will be logged out in 5 seconds due to inactivity',
+            status: 'warning',
+            duration: 5000,
+            isClosable: true,
+        });
+    };
+
+    const resetTimer = () => {
+        clearTimeout(inactiveTimer);
+        clearTimeout(warningTimer);
+        setWarningTimer(setTimeout(showWarning, 25000)); // Show warning after 25 seconds
+        setInactiveTimer(setTimeout(logoutUser, 30000)); // Logout after 30 seconds
+    };
+
+    useEffect(() => {
+        // Set up event listener for keyboard activity
+        window.addEventListener('keydown', resetTimer);
+
+        // Initialize the timers
+        resetTimer();
+
+        // Clean up
+        return () => {
+            window.removeEventListener('keydown', resetTimer);
+            clearTimeout(inactiveTimer);
+            clearTimeout(warningTimer);
+        };
+    }, []);
+
+    return null; 
+};
+
+export default InactivityLogout;
