56f2dbfb70379a5fd4dd7d6870a09c5846c937fd
Alex Wang <wangtcalex@gmail.com>
2023-12-07 15:31:05 -0800

    refac: use useRef to track timer

--- frontend/src/components/InactivityLogout.jsx
+++ frontend/src/components/InactivityLogout.jsx
@@ -1,11 +1,11 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useToast } from '@chakra-ui/react';
 
 const InactivityLogout = () => {
-    const [inactiveTimer, setInactiveTimer] = useState(null);
-    const [warningTimer, setWarningTimer] = useState(null);
-    const [warningShown, setWarningShown] = useState(false);
+    const inactiveTimer = useRef(null);
+    const warningTimer = useRef(null);
+    const warningShownRef = useRef(false);
     const navigate = useNavigate();
     const toast = useToast();
 
@@ -16,8 +16,8 @@ const InactivityLogout = () => {
     };
 
     const showWarning = () => {
-        if (!warningShown) {
-            setWarningShown(true);
+        if (!warningShownRef.current) {
+            warningShownRef.current = true;
             toast({
                 title: 'Inactivity detected',
                 description: 'You will be logged out in 5 seconds due to inactivity',
@@ -29,27 +29,22 @@ const InactivityLogout = () => {
     };
 
     const resetTimer = () => {
-        clearTimeout(inactiveTimer);
-        clearTimeout(warningTimer);
-        setWarningShown(false); // Reset the warning shown status
-        setWarningTimer(setTimeout(showWarning, 25000)); // Show warning after 25 seconds
-        setInactiveTimer(setTimeout(logoutUser, 30000)); // Logout after 30 seconds
+        clearTimeout(inactiveTimer.current);
+        clearTimeout(warningTimer.current);
+        warningShownRef.current = false;
+        warningTimer.current = setTimeout(showWarning, 25000);
+        inactiveTimer.current = setTimeout(logoutUser, 30000);
     };
 
     useEffect(() => {
-        // Set up event listeners for keyboard and mouse activity
         window.addEventListener('keydown', resetTimer);
         window.addEventListener('click', resetTimer);
-
-        // Initialize the timers
         resetTimer();
-
-        // Clean up
         return () => {
             window.removeEventListener('keydown', resetTimer);
             window.removeEventListener('click', resetTimer);
-            clearTimeout(inactiveTimer);
-            clearTimeout(warningTimer);
+            clearTimeout(inactiveTimer.current);
+            clearTimeout(warningTimer.current);
         };
     }, []);
 
