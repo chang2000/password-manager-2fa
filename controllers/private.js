288c042fdd7846932ae338ac9010bcdf774c268e
Alex Wang <wangtcalex@gmail.com>
2023-12-14 14:51:45 -0800

    feat: add poc of manipulating db using gpt

--- controllers/private.js
+++ controllers/private.js
@@ -3,7 +3,7 @@ const jwtDecode = require("../utils/jwtDecode");
 const { errResponse, successResponse } = require("../utils/Response");
 const User = require("../models/user");
 const { encrypt, decrypt } = require("../utils/Crypt");
-const {OpenAI} = require("openai")
+const { OpenAI } = require("openai");
 
 const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 // This function is used to test the route
@@ -126,35 +126,58 @@ async function chat(req, res) {
   console.log("api key", OPENAI_API_KEY);
   const uid = req.user._id;
   const user = await User.findById(uid);
-  console.log(user)
+  console.log(user);
   if (!user.GPTVerified) {
-    console.log('not verified')
+    console.log("not verified");
     errResponse(res, 400, "Not GPT Verified. Please contact admin.");
-    return
+    return;
   }
 
   const { message } = req.body;
 
-  const prompt = `You are a NLP analyzer build-in password manager. If I send you message that contains the information or instruction to delete a password, please return in json format. The instruction is: ${message}: {
-    \"email\": ${user.email},
-    \"website"\: \"website url\",
-  }`;
-  
+  const prompt = `
+  You're a NLP analyzer build-in a password manager, if I send you message that contains the information/instruction/command to delete a password on certain site, return me a sturct answer:
+  "URL=the websirte url". Don't reply anything else.
+  Only when I type 'STOP', you just stop listening to what I say.
+  Here is the user's message: ${message}
+  `;
   try {
     const openai = new OpenAI({
-      apiKey: OPENAI_API_KEY, 
+      apiKey: OPENAI_API_KEY,
     });
-    const chatCompletion = await openai.chat.completions.create({
-      messages: [{ role: 'user', content: `${message}` }],
-      model: 'gpt-3.5-turbo',
-    });
-    res.json({ message: chatCompletion.choices[0].message.content });
 
+    const chatCompletion = await openai.chat.completions.create({
+      messages: [{ role: "user", content: `${prompt}` }],
+      model: "gpt-3.5-turbo",
+    });
+    answer = chatCompletion.choices[0].message.content;
+    site = answer.split("URL=")[1];
+
+    console.log(site);
+    if (site) {
+      try {
+        // Find the user by id and update the passwordEntries array
+        await User.findByIdAndUpdate(uid, {
+          $pull: { passwordEntries: { website: site } },
+        }).then(() => {
+          answer = `Password of ${site} is deleted successfully.`;
+        });
+      } catch (error) {
+        answer = `Failed to delete password of ${site}.`;
+      }
+    }
+
+    res.json({ message: answer });
   } catch (error) {
     console.error("Error calling OpenAI:", error);
     res.status(500).json({ error: "Internal Server Error" });
   }
 }
 
-
-module.exports = { userPage, saveUserPassword, getUserPassword, delPassEntry, chat};
+module.exports = {
+  userPage,
+  saveUserPassword,
+  getUserPassword,
+  delPassEntry,
+  chat,
+};
